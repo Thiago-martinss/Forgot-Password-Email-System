ae20298dbd9567b1f574e716e3ddc9e8275d5c03
Thiago <martinsmthiago@gmail.com>
2025-09-12 21:01:30 -0300

    Server Creation

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
